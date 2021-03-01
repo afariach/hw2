cda315476b9a6e8a5647f0b6d285900c59908fd1
Tanmay Shankar <tanmay.shankar@gmail.com>
2021-03-01 13:10:43 -0500

    Add template for Name and Andrew ID.

--- src/hw2.jl
+++ src/hw2.jl
@@ -2,4 +2,13 @@ module hw2
 
 greet() = print("Hello World!")
 
+
+function studentinfo()
+    info = Dict(
+        "name" => "Brian Jackson",
+        "Andrew ID" => "bjackso2"
+    )
+    return info
+end
+
 end # module
